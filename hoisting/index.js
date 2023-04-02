f076e21d582296bf789dbf9f83cb4725940e8b54
haroldZS <haroldzs1024@gmail.com>
2023-04-02 18:57:35 -0400

    Hoisting

---
+++ hoisting/index.js
@@ -0,0 +1,24 @@
+/*
+ELEVACIÓN
+var nameOfDog; //undefined
+*/
+
+console.log(nameOfDog);
+var nameOfDog = 'Elmo';
+console.log(nameOfDog);
+
+/*
+ELEVACIÓN
+
+var elmo; // Undefined
+function nameOfDog(){
+    console.log(`El mejor perrito es ${elmo}`);
+}
+*/
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito';
