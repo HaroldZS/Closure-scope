fed6bdb9edef7fa52de4e3aaa5400177b2577bb5
haroldZS <haroldzs1024@gmail.com>
2023-04-02 18:45:19 -0400

    Closures

---
+++ src/scope/moneyBox.js
@@ -0,0 +1,31 @@
+/*
+function moneyBox(coins){
+    let saveCoins = 0;
+    saveCoins += coins;
+    console.log(`MoneyBox: $${saveCoins}`);
+}
+
+moneyBox(5);
+moneyBox(5);
+*/
+
+function moneyBox() {
+  let saveCoins = 0;
+
+  function countCoins(coins){
+    saveCoins += coins;
+    console.log(`MoneyBox: $${saveCoins}`)
+  }
+
+  return countCoins;
+}
+
+const newMoneyBox = moneyBox();
+newMoneyBox(5);
+newMoneyBox(5);
+newMoneyBox(15);
+
+const moneyBoxAna = moneyBox();
+moneyBoxAna(10);
+moneyBoxAna(20);
+moneyBoxAna(5);
